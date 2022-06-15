b9751889d2767432903790d562b2d812415b8c7a
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2022-06-15 17:10:28 -0300

    2022/06/15 update

---
+++ React/Introdução ao React/introducao_react/src/ComponentA.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ComponentA(props) {
+    return (
+        <div>
+            Componente A<div>{props.children}</div>
+        </div>
+    );
+}
+
+export default ComponentA;
